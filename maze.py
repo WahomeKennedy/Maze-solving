744b661b51855c8042825b296039897b90b26ffd
WahKen <120699734+WahomeKennedy@users.noreply.github.com>
2023-02-04 21:14:57 +0300

    Update maze solving

---
+++ maze.py
@@ -0,0 +1,42 @@
+def solve_maze(maze, startX, startY, endX, endY):
+    visited = []
+    for x, line in enumerate(maze):
+        visited.append([])
+        for cell in line:
+            visited[x].append(cell)
+    
+    visited[startX][startY] = 2
+    currX = startX
+    currY = startY
+    
+    options = []
+
+    while currX != endX or currY != endY:
+        if currX+1 < len(maze[currX]) and maze[currX+1][currY] == 0 and visited[currX+1][currY] !=2:
+            options.append(Point(currX+1, currY))
+
+        if currX-1 >= 0 and maze[currX-1][currY] == 0 and visited[currX-1][currY] !=2:
+            options.append(Point(currX-1, currY))
+        
+        if currY+1 < len(maze[currX]) and maze[currX][currY+1] == 0 and visited[currX][currY+1] !=2:
+            options.append(Point(currX, currY+1))
+        
+        if currY-1 >= 0 and maze[currX][currY-1] == 0 and visited[currX][currY-1] !=2:
+            options.append(Point(currX, currY-1))
+
+        if len(options) == 0: # no options means nowhere to go
+            return None
+        
+        go_to_point = options.pop()
+        currX = go_to_point.x
+        currY = go_to_point.y
+        visited[currX][currY] = 2
+
+        return visited
+
+    """class Point:
+        def __init__(self, x,y):
+            self.x = x
+            self.y = y"""
+
+
